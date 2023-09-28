399ddafc92793f586b5a0419bdf14b3d24e58ac6
Hestice <custillano@gmail.com>
2023-09-28 19:56:04 +0800

    just learned loading mechanics

---
+++ app/cart/loading.tsx
@@ -0,0 +1,5 @@
+import MainLoadingScreen from "@/components/main-loading-screen"
+
+export default function Loading(){
+    return <MainLoadingScreen/>
+}
---
+++ components/main-loading-screen.tsx
@@ -0,0 +1,3 @@
+export default function MainLoadingScreen() {
+    return <p>Loading...</p>
+}
